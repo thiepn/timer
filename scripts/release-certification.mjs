d54e1446e1b4847e8b8af7ecd24ebafce0b3af3f
thiepn <junsosmart@gmail.com>
2026-09-23 21:55:30 +0200

    Visual V4: bump PWA cache for Home redesign

--- scripts/release-certification.mjs
+++ scripts/release-certification.mjs
@@ -17,7 +17,7 @@ if (/user-scalable\s*=\s*no/i.test(html) || /maximum-scale\s*=\s*1/i.test(html))
 
 const sw = read('sw.js');
 const cacheMatch = sw.match(/const CACHE = 'thiepn-timer-v(\d+)'/);
-if (!cacheMatch || Number(cacheMatch[1]) !== 19) fail('service-worker cache generation must be v19');
+if (!cacheMatch || Number(cacheMatch[1]) !== 20) fail('service-worker cache generation must be v20');
 
 const app = read('src/app.js');
 const appImports = [...app.matchAll(/from\s+['"](\.\/[^'"]+\.js)['"]/g)].map((m) => `./src/${m[1].replace(/^\.\//, '')}`);
